691c1f5b33b9a22ca069d740fa30ac38f3c1a6d2
Pawel Kacprzak <pawel.kacprzak.89@gmail.com>
2013-07-10 16:08:39 +0000

    Reading from external file

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var buf = fs.readFileSync('index.html');
+var s = buf.toString('utf-8');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(s);
 });
 
 var port = process.env.PORT || 5000;
